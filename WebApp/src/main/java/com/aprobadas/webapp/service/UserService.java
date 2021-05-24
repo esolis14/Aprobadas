002de98bf58194c716482ec83f2b8c5275de6e38
esolis14 <esolis003@ikasle.ehu.eus>
2021-05-24 13:08:41 +0200

    fix(1): inicializacion de PasswordEncoder añadida a constructor

--- WebApp/src/main/java/com/aprobadas/webapp/service/UserService.java
+++ WebApp/src/main/java/com/aprobadas/webapp/service/UserService.java
@@ -25,12 +25,13 @@ public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-    private BCryptPasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
     public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Override
